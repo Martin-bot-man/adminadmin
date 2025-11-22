850b4f9012ce43fde4409993c3c70fa3f0a5dbf6
Martin-bot-man <martinwere14@gmail.com>
2025-11-22 06:55:34 +0300

    created views.py with queryset and serializer_class included

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import MyModel
+from .serializers import MyModelSerializer
+
+
+class BookViewCreateAPIView(generics.ListCreateAPIView)
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.
